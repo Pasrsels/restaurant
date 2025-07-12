afa42c9a047ecccdfdfb65efa0560fb70a8acb4e
= <=>
2025-07-12 10:31:10 +0200

    Enhance templates and models: add styling adjustments, improve sidebar layout, and implement string representation for Branch model

--- users/models.py
+++ users/models.py
@@ -49,6 +49,8 @@ class Company(models.Model):
 class Branch(models.Model):
     name = models.CharField(max_length=60, default='Main')
 
+    def __str__(self):
+        return self.name
 class User(AbstractUser):
     
     USER_ROLES = (
