3ca4eca6c888b85298317a97bcef881878c9cc11
= <=>
2025-07-22 10:04:42 +0200

    Refactor inventory views and enhance POS template styles

--- inventory/views.py
+++ inventory/views.py
@@ -2198,7 +2198,7 @@ def add_dish(request): # didn't change the name of the template, it caters for b
     dish_form = DishForm()
     
     if request.method == 'GET':
-        r_m = Product.objects.filter(raw_material=True, minor_raw_material__branch=request.user.branch)
+        r_m = Product.objects.filter(raw_material=True, branch=request.user.branch)
         return render(request, 'inventory/ingredient_form.html', 
             {
                 'r_m':r_m,
@@ -2276,7 +2276,7 @@ def add_dish(request): # didn't change the name of the template, it caters for b
                         minor_raw_material=raw_material,
                         quantity=item.get('quantity'),
                         cost=item.get('cost'),
-                        branch=request.user.branch
+                        # branch=request.user.branch
                     )
 
         except Exception as e:
